91908c0ec85b6cb03c3450e0e7be0fe642928337
desmondlsl <68117020+desmondlsl@users.noreply.github.com>
2021-04-09 01:30:19 +0800

    Change title

--- src/App.vue
+++ src/App.vue
@@ -11,26 +11,27 @@
           width="40"
         />
 
-        <v-img
+        <h2>hackust2021-plasticity</h2>
+        <!-- <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
