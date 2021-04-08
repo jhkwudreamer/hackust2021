c3a68e08c81cf4cca37bf6969649fee5f8133f0b
samsonwong522 <samsonwong522@gmail.com>
2021-04-09 01:47:47 +0800

    Add StreetView

--- src/App.vue
+++ src/App.vue
@@ -12,42 +12,24 @@
         />
 
         <h2>hackust2021-plasticity</h2>
-        <!-- <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
       </div>
-
-      <v-spacer></v-spacer>
-
-      <!-- <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn> -->
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <StreetView />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
+import StreetView from "./components/StreetView";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    StreetView,
   },
 
   data: () => ({
---
+++ src/components/StreetView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div id="pano"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function () {
+    const fenway = { lat: 22.3095008, lng: 114.2377189 };
+    // const panorama =
+    // eslint-disable-next-line no-undef
+    new google.maps.StreetViewPanorama(document.getElementById("pano"), {
+      position: fenway,
+      pov: {
+        heading: 180,
+        pitch: 10,
+      },
+      zoomControl: false,
+      imageDateControl: false,
+      linksControl: true, // ?
+      fullscreenControl: false,
+      addressControl: false,
+    });
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+#pano {
+  display: flex;
+  width: 100%;
+}
+</style>
