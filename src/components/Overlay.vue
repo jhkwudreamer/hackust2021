b1f16d7b4753779baff5af8ceca894da44b8240b
samsonwong522 <samsonwong522@gmail.com>
2021-04-09 02:24:42 +0800

    Fix overlay

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -16,8 +16,8 @@ export default {
 
 <style scoped>
 .container {
-  width: 100%;
-  height: 100%;
+  /* width: 100%;
+  height: 100%; */
   position: absolute;
   z-index: 10000;
 }
--- src/components/StreetView.vue
+++ src/components/StreetView.vue
@@ -26,7 +26,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .container {
   display: flex;
   width: 100%;
