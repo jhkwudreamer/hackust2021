26d83bb30cf7d1e082dc75497f285799e4eb6e6d
desmondlsl <68117020+desmondlsl@users.noreply.github.com>
2021-04-15 23:51:43 +0800

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
+        <v-app-bar-nav-icon
+          @click.stop="
+            navDrawer.isNavDrawerOpened = !navDrawer.isNavDrawerOpened
+          "
+        ></v-app-bar-nav-icon>
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -13,7 +18,100 @@
 
         <h2>hackust2021-plasticity</h2>
       </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="hidden-md-and-down">
+        <template v-if="user.isSignedIn">
+          <v-menu offset-y open-on-hover>
+            <template v-slot:activator="{ on, attrs }">
+              <span> </span>
+              <v-btn v-bind="attrs" v-on="on">
+                <v-avatar>
+                  <v-img
+                    v-if="user.userData.photoURL"
+                    :src="user.userData.photoURL"
+                  />
+                </v-avatar>
+                {{ user.userData.displayName }}
+                <v-icon>fa-chevron-down</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <v-list-item-title>
+                  <v-btn text :to="{ name: 'MyAccount' }">My Account</v-btn>
+                </v-list-item-title>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title>
+                  <v-btn text @click.prevent="signOut">Sign out</v-btn>
+                </v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+        <template v-else>
+          <v-btn :to="{ name: 'SignIn' }">Sign in</v-btn>
+        </template>
+      </div>
     </v-app-bar>
+
+    <v-navigation-drawer app v-model="navDrawer.isNavDrawerOpened">
+      <v-list nav dense>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>hackust2021-plasticity</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-group
+          class="hidden-lg-and-up"
+          v-if="user.isSignedIn"
+          v-model="navDrawer.isAccountOpened"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-avatar>
+              <v-img :src="user.userData.photoURL" />
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ user.userData.displayName }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item :to="{ name: 'MyAccount' }">
+            <v-list-item-icon><v-icon>fa-user</v-icon></v-list-item-icon>
+            <v-list-item-title>My Account</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click.prevent="signOut">
+            <v-list-item-icon>
+              <v-icon>fa-sign-out-alt</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Sign Out</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+      <v-divider />
+      <v-list nav dense>
+        <v-list-item :to="{ name: 'Home' }" exact>
+          <v-list-item-icon><v-icon>fa-home</v-icon></v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item v-if="!user.isSignedIn" :to="{ name: 'SignIn' }">
+          <v-list-item-icon><v-icon>fa-sign-in-alt</v-icon></v-list-item-icon>
+          <v-list-item-title>Sign In</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item :to="{ name: 'About' }">
+          <v-list-item-icon><v-icon>fa-info-circle</v-icon></v-list-item-icon>
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view />
@@ -28,7 +126,24 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    drawer: null,
+    navDrawer: {
+      isNavDrawerOpened: null,
+      isAccountOpened: false,
+    },
+    user: {
+      isSignedIn: true,
+      userData: {
+        displayName: "JW",
+        photoURL: require("@/assets/userPhoto.png"),
+      },
+    },
   }),
+
+  methods: {
+    signOut() {
+      return;
+    },
+  },
 };
 </script>
